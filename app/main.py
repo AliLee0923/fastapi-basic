c9b42ebe5785260426e8638015ce159274bd1d00
Ali <ali.lee.0923@gmail.com>
2025-03-30 09:16:01 -0700

    complete

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from routes import auth, post
+from db import Base, engine
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(post.router)
+
+Base.metadata.create_all(bind=engine)
